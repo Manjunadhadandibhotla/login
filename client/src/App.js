d6cc1476e2bffd3893c9a6101df18c061361be0b
Manjunadha_dandibhotla <manjunadhadandibhotla@gmail.com>
2023-09-09 01:38:11 +0530

    added most of the files. chnges needed in authnetication

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React_App</h1>
-    </div>
-  );
-}
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
 
-export default App;
+
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Username></Username>
+    },
+    {
+        path: '/register',
+        element: <Register></Register>
+    },
+    {
+        path: '/password',
+        element: <Password></Password>
+    },
+    {
+        path: '/profile',
+        element:<Profile></Profile>
+    },
+    {
+        path: '/recovery',
+        element: <Recovery></Recovery>
+    },
+    {
+        path: '/reset',
+        element: <Reset></Reset>
+    },
+    {
+        path: '*',
+        element: <PageNotFound></PageNotFound>
+    },
+])
+
+export default function App() {
+    return (
+        <main>
+            <RouterProvider router={router}></RouterProvider>
+        </main>
+    )}
